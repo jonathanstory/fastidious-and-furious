d1e6bbd9b40aa1610e1c73368321c46f4396ae16
jonathanstory <55607242+jonathanstory@users.noreply.github.com>
2025-06-09 15:00:06 -0400

    AI Updates
    Fixed tracking and movement of AI

--- Assets/PROMETEO - Car Controller/Scripts/PrometeoCarController.cs
+++ Assets/PROMETEO - Car Controller/Scripts/PrometeoCarController.cs
@@ -13,6 +13,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class PrometeoCarController : MonoBehaviour
 {
